9d985bad51b86fbb67b7ddbf3147cf18d21231bd
James Ashton <ashtonj@gmail.com>
2020-09-02 23:48:11 -0400

    Test var create & set code

--- randomuser.py
+++ randomuser.py
@@ -4,15 +4,22 @@ import random
 
 from createsend import Subscriber, Client, CreateSend, List
 a_test_client_auth = config.a_test_client_auth
-account_auth = config.a_test_client_auth
+account_auth = config.account_auth
 
-
-
-
-def create_new_list(clientid, list_name="Main List"):
+def create_new_list(clientid, list_name="Main List 7"):
     my_list = List(account_auth)
-    my_list.create(clientid, list_name, "", False, "")
+    list_id=my_list.create(clientid, list_name, "", False, "")
+    return list_id
 
+def create_custom_fields(field_name,options,list_id="2345accabd471c4c45e5fc463c8353a1"):
+    my_list = List(account_auth,list_id)
+    create_custom_fields = my_list.create_custom_field(field_name=field_name,data_type="MultiSelectOne",options=options,visible_in_preference_center=True)
+
+#new_list_id=create_new_list(clientid="ae803b630f09179e016db499a401fdc6")
+
+#print("Your list ID is" + new_list_id)
+
+#print(create_custom_fields)
 
 def print_random_name():
     url = "https://randomuser.me/api?results=5&nat=us,gb"
@@ -21,35 +28,41 @@ def print_random_name():
     #print(data)
     return(data)
 
-#def get_main_list():
 
-my_client=Client(a_test_client_auth)
-client_lists = my_client.lists()
-print(client_lists)
+# my_client = Client(account_auth)
+# client_lists = my_client.lists()
+# print(client_lists)
 
 
-'''
+
 my_names=print_random_name()
 
 random_subscriber={}
 subscriber_list=[]
 
+
+test_field_name="Main Hobby"
 possible_interests=["getting drunk","playing cards","rollerblading"]
 
+#fields_created = create_custom_fields(field_name=test_field_name,options=possible_interests)
+#print("You created the following entry" + fields_created)
+
+
+#NEED TO CREATE THE CUSTOM FIELDS first
 for i in my_names['results']:
     print(i['name']['first']+" "+i['name']['last'])
     random_subscriber={}
     random_subscriber.update(Name = i['name']['first']+" "+i['name']['last'])
     random_subscriber.update(EmailAddress = i['email'])
     random_subscriber.update(ConsentToTrack = "Yes")
-    random_subscriber.update(CustomFields = [{"Key":"interests","Value":random.choice(possible_interests)}])
+    random_subscriber.update(CustomFields = [{"Value":random.choice(possible_interests),"Key":test_field_name}])
+    #print(random_subscriber)
     subscriber_list.append(random_subscriber)
 
 
 #print(subscriber_list)
 #create_new_list(clientid="9cde80d058d8e4955a076d33b6ec2294")
 
-
+print(subscriber_list)
 my_subscriber = Subscriber(account_auth)
-my_subscriber.import_subscribers("0561ad7fe4c247083dd71ca327bd1e1d", subscriber_list, True, False, True)
-'''
+my_subscriber.import_subscribers("2345accabd471c4c45e5fc463c8353a1", subscriber_list, True, False, True)
